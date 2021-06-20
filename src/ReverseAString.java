9b4cfb79356269be3af3c3a5a1752ad3bfc359d5
Deepak Kumar <53215180+deepakkumar2604@users.noreply.github.com>
2021-06-20 13:25:44 +0530

    Adding Reverse string file - 3rd Commit

--- src/ReverseAString.java
+++ src/ReverseAString.java
@@ -1,31 +1,34 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 public class ReverseAString {
+    public static String Name;
 
+    public ReverseAString() {
+    }
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         System.out.println("hello GIT");
-        String Name = "Deepak Kumar";
-
+        Name = "Deepak Kumar";
         ReverseAString(Name);
-
+        ReverseAStringUsingForLoop(Name);
     }
 
-    public static void ReverseAString(String s){
-
-
-        StringBuffer sb= new StringBuffer(s);
+    public static void ReverseAString(String s) {
+        StringBuffer sb = new StringBuffer(s);
         System.out.println(sb.reverse());
-
-
         System.out.println("-----------------------------------------------------");
-
-
-
-
-
     }
 
+    public static void ReverseAStringUsingForLoop(String s) {
+        String reverse = "";
 
+        for(int i = Name.length() - 1; i >= 0; --i) {
+            reverse = reverse + Name.charAt(i);
+        }
 
-
+        System.out.println(reverse);
+    }
 }
